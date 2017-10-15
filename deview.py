e6056e5ceef916f4ed00e7fe391404f0d19f3d30
Jaeseok Yoon <yjaeseok@gmail.com>
2017-10-15 16:54:42 +0900

    Add comments

--- deview.py
+++ deview.py
@@ -1,53 +1,52 @@
-import requests, random
+import requests
 from bs4 import BeautifulSoup
 
 
 def spider(max_pages):
-    skipCount = 0
+    skip_count = 0
     schedule = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
 
     print(str(max_pages))
     page = 1
     while page <= max_pages:
-    #if page <= max_pages:
-    #    page = 3890
         print("request page ", page)
         url = "https://deview.kr/2017/pick/" + str(page)
-        sourceCode = requests.get(url)
-        soup = BeautifulSoup(sourceCode.text, 'lxml')
-
+        source_code = requests.get(url)
+        soup = BeautifulSoup(source_code.text, 'lxml')
+        
+        # day1 정보 입력 여부 확인
         day1 = False
-
         h3s = soup.find_all('h3')
         for h3 in h3s:
             h3contents = str(h3.contents[0])
             if h3contents.startswith("DAY 1"):
                 day1 = True
 
+        # day1 이 입력 되어 있다면
         if day1:
-            track_infos = soup.find_all('dd', class_='track_info')
-            print("length : ", len(track_infos))
-
-            for idx, track_info in enumerate(track_infos):
+            track_info_list = soup.find_all('dd', class_='track_info')
+            for idx, track_info in enumerate(track_info_list):
                 track_no_object = track_info.find('p', class_='track_no')
                 if track_no_object is None:
                     continue
                 track_no = str(track_no_object.contents[0])
+
+                # 키노트 제외
                 if 'ALL TRACKS' == track_no:
                     continue
+                # Day 2 제외
                 if idx >= 6:
                     continue
 
                 schedule[idx][int(track_no[5]) - 1] = schedule[idx][int(track_no[5]) - 1] + 1
         else:
-            skipCount = skipCount + 1
+            skip_count = skip_count + 1
 
         page = page + 1
 
     print(str(schedule))
-    print("skipCount", skipCount)
+    print("skip_count", skip_count)
+
 
 if __name__ == "__main__":
     spider(5121)
-
-    #random.randrange(1, 5121))
